ab2bbca58ae2ebfb8cecda0d027cd00c561d854d
De General <aubynsamuel05@gmail.com>
2026-02-07 10:52:22 +0000

    Update Material3 to 1.5.0-alpha11 and migrate to MaterialExpressiveTheme

--- app/src/main/java/com/veivek/monthscalculator/ui/theme/Theme.kt
+++ app/src/main/java/com/veivek/monthscalculator/ui/theme/Theme.kt
@@ -2,7 +2,9 @@ package com.veivek.monthscalculator.ui.theme
 
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.MaterialExpressiveTheme
+import androidx.compose.material3.MotionScheme
 import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
@@ -20,18 +22,9 @@ private val LightColorScheme = lightColorScheme(
     primary = Purple40,
     secondary = PurpleGrey40,
     tertiary = Pink40
-
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
 )
 
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
 @Composable
 fun MonthsCalculatorTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
@@ -49,9 +42,10 @@ fun MonthsCalculatorTheme(
         else -> LightColorScheme
     }
 
-    MaterialTheme(
+    MaterialExpressiveTheme(
         colorScheme = colorScheme,
         typography = Typography,
-        content = content
+        content = content,
+        motionScheme = MotionScheme.expressive()
     )
 }
